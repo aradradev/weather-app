70a38f759c9b4faa225fc9df096b9267c9b131db
Abdoul <abdoulramanediallo44@gmail.com>
2023-07-28 16:57:01 +0000

    add setup react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TopButtons from './components/TopButtons';
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-24 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400 ">
+      <TopButtons />
+    </div>
+  );
+}
+
+export default App;
